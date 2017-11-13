bed854d4b153c9e6af041b24d7af1e8b2dde468e
mason.chae <mason.chae@nsuslab.com>
2017-11-13 12:53:36 +0900

    test 1
    test 2

--- akka-cluster-test/table/src/main/scala/com/nsuslab/test/akkaclustertest/table/service/TableService.scala
+++ akka-cluster-test/table/src/main/scala/com/nsuslab/test/akkaclustertest/table/service/TableService.scala
@@ -14,6 +14,8 @@ class TableService extends Actor with ActorLogging {
   val cluster = Cluster(system)
 
   cluster registerOnMemberUp {
+    // 1
+    // 2
     // Do something when this node become a member-up in a cluster
   }
 
