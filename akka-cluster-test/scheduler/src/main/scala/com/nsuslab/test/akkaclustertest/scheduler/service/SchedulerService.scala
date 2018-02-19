94f2e14a490388fbb011e1b40000e5734fc0c413
mason.chae <mason.chae@nsuslab.com>
2018-02-19 11:39:37 +0900

    CAI-003 gogogo 03qq

--- akka-cluster-test/scheduler/src/main/scala/com/nsuslab/test/akkaclustertest/scheduler/service/SchedulerService.scala
+++ akka-cluster-test/scheduler/src/main/scala/com/nsuslab/test/akkaclustertest/scheduler/service/SchedulerService.scala
@@ -4,11 +4,15 @@ import scala.concurrent.duration._
 import akka.cluster.Cluster
 import akka.actor.{Actor, ActorLogging, ActorSystem}
 import akka.cluster.ClusterEvent._
-import com.hazelcast.core.Hazelcast
+import com.hazelcast.core.{Hazelcast, IMap}
 import com.nsuslab.test.akkaclustertest.common.message._
 import java.lang.management.{ManagementFactory, MemoryMXBean}
 import javax.management.ObjectName
 import javax.management.remote.{JMXConnectorFactory, JMXServiceURL}
+
+import akka.cluster.sharding.ClusterSharding
+import com.nsuslab.test.akkaclustertest.common.database.entity.GameDataInfo
+import com.nsuslab.test.akkaclustertest.common.shard.GameSharding
 
 import scala.concurrent.ExecutionContext
 
@@ -61,33 +65,33 @@ class SchedulerService extends Actor with ActorLogging {
     case evt: MemberEvent =>
       log.info(" *** MemberEvent: {}", evt)
     case msg : StartSystem =>
-      import scala.collection.JavaConverters._
-
-      log.info(" *** Receive a message: {}", msg)
-      val jmxPeer = "127.0.0.1:5011"
-      val jmxUrl = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://%s/jmxrmi".format(jmxPeer))
-
-      val aaa = Map[String, AnyRef](javax.management.remote.JMXConnector.CREDENTIALS -> Array[String]("admin","q1w2e3"))
-
-      val jmxc = JMXConnectorFactory.connect(jmxUrl, aaa.asJava)
-      val connection = jmxc.getMBeanServerConnection
-
-      println("\nDomains:")
-      val domains = connection.getDomains
-      for (domain <- domains) {
-        println("\tDomain = " + domain)
-      }
-
-      println("\nMBeanServer default domain = " + connection.getDefaultDomain)
-
-      println("\nMBean count = " + connection.getMBeanCount)
-      println("\nQuery MBeanServer MBeans:")
-
-      val names = connection.queryNames(null, null)
-
-      for (name <- names.toArray) {
-        println("\tObjectName = " + name.toString)
-      }
+//      import scala.collection.JavaConverters._
+//
+//      log.info(" *** Receive a message: {}", msg)
+//      val jmxPeer = "127.0.0.1:5011"
+//      val jmxUrl = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://%s/jmxrmi".format(jmxPeer))
+//
+//      val aaa = Map[String, AnyRef](javax.management.remote.JMXConnector.CREDENTIALS -> Array[String]("admin","q1w2e3"))
+//
+//      val jmxc = JMXConnectorFactory.connect(jmxUrl, aaa.asJava)
+//      val connection = jmxc.getMBeanServerConnection
+//
+//      println("\nDomains:")
+//      val domains = connection.getDomains
+//      for (domain <- domains) {
+//        println("\tDomain = " + domain)
+//      }
+//
+//      println("\nMBeanServer default domain = " + connection.getDefaultDomain)
+//
+//      println("\nMBean count = " + connection.getMBeanCount)
+//      println("\nQuery MBeanServer MBeans:")
+//
+//      val names = connection.queryNames(null, null)
+//
+//      for (name <- names.toArray) {
+//        println("\tObjectName = " + name.toString)
+//      }
 //      val objName: ObjectName = new ObjectName("akkaclustertest.tables", {
 //        import scala.collection.JavaConverters._
 //        new java.util.Hashtable(
@@ -100,23 +104,23 @@ class SchedulerService extends Actor with ActorLogging {
 //      println(" ** " + connection.getMBeanInfo(objName))
 //      println(" ** " + connection.invoke(objName, "GetActorPath", null, null))
 
-      val memProxy = ManagementFactory.newPlatformMXBeanProxy(connection, ManagementFactory.MEMORY_MXBEAN_NAME, classOf[MemoryMXBean])
-      println("\n *** %s: %s\n".format(jmxPeer, memProxy.getHeapMemoryUsage))
-      jmxc.close()
+//      val memProxy = ManagementFactory.newPlatformMXBeanProxy(connection, ManagementFactory.MEMORY_MXBEAN_NAME, classOf[MemoryMXBean])
+//      println("\n *** %s: %s\n".format(jmxPeer, memProxy.getHeapMemoryUsage))
+//      jmxc.close()
 
-//      val shardRegion = ClusterSharding(context.system).shardRegion(GameSharding.shardName)
-//      val gameInfo: IMap[Long, GameDataInfo] = hazelcast.getMap("GAMEDATA_INFO")
-//
-//      println("  ----  " + gameInfo.values().size())
-//
-//      gameInfo.values().forEach {
-//        item =>
-//          println(s"--------${item.getGameId}")
-//          shardRegion ! GameEnvelopeMessage(item.getGameId, item.getGameId,
-//                            CreateGameMessage(item.getGameId, item.getGameType, item.getMaxCount, item.getPlayerCount))
-//      }
+      val shardRegion = ClusterSharding(context.system).shardRegion(GameSharding.shardName)
+      val gameInfo: IMap[Long, GameDataInfo] = hazelcast.getMap("GAMEDATA_INFO")
 
-      context.system.scheduler.scheduleOnce(10.second, self, StartSystem())
+      println("  ----  " + gameInfo.values().size())
+
+      gameInfo.values().forEach {
+        item =>
+          println(s"--------${item.getGameId}")
+          shardRegion ! GameEnvelopeMessage(item.getGameId, item.getGameId,
+                            CreateGameMessage(item.getGameId, item.getGameType, item.getMaxCount, item.getPlayerCount))
+      }
+
+//      context.system.scheduler.scheduleOnce(10.second, self, StartSystem())
     case msg =>
       log.warning(" *** Receive a unknown message: {} ", msg)
   }
